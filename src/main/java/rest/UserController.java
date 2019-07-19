343b5898d2abc14d7f2af77bb8d4ecb2cddb2cbb
Dan Hayes <dan.hayes1996@gmail.com>
2019-07-19 11:35:37 +0100

    added HTTP methods in UserController

--- src/main/java/rest/UserController.java
+++ src/main/java/rest/UserController.java
@@ -1,6 +1,9 @@
 package rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
@@ -12,26 +15,31 @@ public class UserController {
 	@Inject
 	private UserService service;
 	
+	@GET
 	@Path("/allUsers")
 	public String getAllUsers() {
 		return service.getAllUsers();
 	}
-	
+
+	@GET
 	@Path("/getUser/{id}")
 	public String getUser(@PathParam("id") long id) {
 		return service.getUser(id);
 	}
 
+	@POST
 	@Path("/createUser")
 	public String createUser(String user) {
 		return service.createUser(user);
 	}
 
+	@DELETE
 	@Path("/deleteUser/{id}")
 	public String deleteUser(@PathParam("id") long id) {
 		return service.deleteUser(id);
 	}
 
+	@POST
 	@Path("/updateUser/{id}")
 	public String updateUser(@PathParam("id") long id, String user) {
 		return service.updateUser(id, user);
