5ce717a7be181785e0f7cf14ed4331ddf7db2ed4
dan hayes <dan.hayes1996@gmail.com>
2019-07-29 22:47:40 +0100

    added table relationships

--- src/main/java/service/ReviewService.java
+++ src/main/java/service/ReviewService.java
@@ -4,7 +4,7 @@ public interface ReviewService {
 
 	public String getAllReviews();
 	public String getReview(long id);	
-	public String createReview(String review);
+	public String createReview(long userId, long gameId, String review);
 	public String deleteReview(long id);
 	public String updateReview(long id, String review);
 }
